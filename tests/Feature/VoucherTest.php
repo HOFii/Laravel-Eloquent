95923e9d2a54f53218003913910539a2e268d3f5
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-22 20:54:38 +0700

    modify tests/Feature

---
+++ tests/Feature/VoucherTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Voucher;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VoucherTest extends TestCase
+{
+    public function testCreateVoucher()
+    {
+        $voucher = new Voucher();
+        $voucher->name = "Sample Voucher";
+        $voucher->voucher_code = "23414124214";
+        $voucher->save();
+
+        self::assertNotNull($voucher->id);
+
+    }
+}
