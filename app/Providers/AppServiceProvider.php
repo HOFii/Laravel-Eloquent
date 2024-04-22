dad3d0cafb7f7634826393e5e5918f2b81dd346e
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-22 20:51:03 +0700

    modify file AppServiceProviders.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Models\Customer;
+use App\Models\Product;
+use App\Models\Voucher;
+use Illuminate\Database\Eloquent\Relations\Relation;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        DB::listen(function ($query){
+            Log::info("Query : {$query->sql}");
+        });
     }
 }
