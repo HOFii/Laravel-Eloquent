69d8e840a8e3b77162ff2a317c8956b26c12d64c
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-23 18:47:40 +0700

    added new file tests

--- tests/Feature/VoucherTest.php
+++ tests/Feature/VoucherTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Voucher;
+use Database\Seeders\VoucherSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,6 +18,27 @@ class VoucherTest extends TestCase
         $voucher->save();
 
         self::assertNotNull($voucher->id);
+    }
+    public function testCreateVoucherUUID()
+    {
+        $voucher = new Voucher();
+        $voucher->name = "Sample Voucher";
+        $voucher->save();
 
+        self::assertNotNull($voucher->id);
+        self::assertNotNull($voucher->voucher_code);
+    }
+    public function testSoftDelete()
+    {
+        $this->seed(VoucherSeeder::class);
+
+        $voucher = Voucher::where('name', '=', 'Sample Voucher')->first();
+        $voucher->delete();
+
+        $voucher = Voucher::where('name', '=', 'Sample Voucher')->first();
+        self::assertNull($voucher);
+
+        $voucher = Voucher::withTrashed()->where('name', '=', 'Sample Voucher')->first();
+        self::assertNotNull($voucher);
     }
 }
