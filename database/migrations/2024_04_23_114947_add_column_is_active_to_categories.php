e4fd3c08e172f8c79834e19a9d4e39e68278f741
Gusti Alifiraqsha <alifiraqshaakbar@gmail.com>
2024-04-24 21:01:33 +0700

    added database/migrations

--- database/migrations/2024_04_23_114947_add_column_is_active_to_categories.php
+++ database/migrations/2024_04_23_114947_add_column_is_active_to_categories.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->boolean("is_active")->nullable()->default(false);
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('categories', function (Blueprint $table) {
-            //
+            $table->dropColumn("is_active");
         });
     }
 };
